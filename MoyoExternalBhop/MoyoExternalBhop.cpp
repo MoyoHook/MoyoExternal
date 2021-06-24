39342659d420b0cb01a0babf1249b31e218e4737
Moyo <moyo@moyomo.xyz>
2021-06-24 18:37:27 +0200

    v0.2
    improved process finding

--- MoyoExternalBhop/MoyoExternalBhop.cpp
+++ MoyoExternalBhop/MoyoExternalBhop.cpp
@@ -6,13 +6,13 @@ MemMan MemClass;
 
 int main()
 {
-    float version = 0.1;
+    float version = 0.2;
 
     std::cout << "  __  __                   ____  _                 \n |  \\/  | ___  _   _  ___ | __ )| |__   ___  _ __  \n | |\\/| |/ _ \\| | | |/ _ \\|  _ \\| '_ \\ / _ \\| '_ \\ \n | |  | | (_) | |_| | (_) | |_) | | | | (_) | |_) |\n |_|  |_|\\___/ \\__, |\\___/|____/|_| |_|\\___/| .__/ \n               |___/                        |_|    \n--------------------------------------------------\nversion: " << version << std::endl;
     
     std::cout << "\n[+] getting CSGO Process..." << std::endl;
 
-    DWORD process, client, localPlayer;
+    DWORD process, client, localPlayer = NULL;
     BYTE flag;
 
     //offsets
@@ -26,39 +26,43 @@ int main()
     }
     catch(const std::exception& e){
         std::cout << "[!] ERROR: " << e.what() << std::endl;
-    }
+    } 
 
     if (client == NULL) {
         std::cout << "[!] CSGO process not found!" << std::endl;
-    }
-    else {
-        std::cout << "[+] Process found! Address: " << std::hex << client << std::endl;
-        localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
-        if (localPlayer == NULL) {
-            std::cout << "[+] Can't find Local Player. Waiting for gamestatus update..." << std::endl;
-            while (localPlayer == NULL)
-            {
-                localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
-            }
-        }
-        std::cout << "[+] Local Player Address: " << std::hex << localPlayer << std::endl;
-
-        //loop
-        while (true)
+        std::cout << "[+] Waiting for CSGO Process..." << std::endl;
+        while (client == NULL)
         {
-            if (GetAsyncKeyState(VK_INSERT)) {
-                localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
-                std::cout << "[+] New Player Address: " << std::hex << localPlayer << std::endl;
-                while (GetAsyncKeyState(VK_INSERT)){}
-            }
-            flag = MemClass.readMem<BYTE>(localPlayer + m_fFlags);
-            if (GetAsyncKeyState(VK_SPACE) && flag & (1 << 0)) {
-                MemClass.writeMem<DWORD>(client + dwForceJump, 6);
-            }
-
-            Sleep(1);
+            process = MemClass.getProcess("csgo.exe");
+            client = MemClass.getModule(process, "client.dll");
+            Sleep(100);
         }
-
     }
 
+    std::cout << "[+] Process found! Address: " << std::hex << client << std::endl;
+    localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
+    if (localPlayer == NULL) {
+        std::cout << "[+] Can't find Local Player. Waiting for gamestatus update..." << std::endl;
+        while (localPlayer == NULL)
+        {
+            localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
+        }
+    }
+    std::cout << "[+] Local Player Address: " << std::hex << localPlayer << std::endl;
+    
+    //loop
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_INSERT)) {
+            localPlayer = MemClass.readMem<DWORD>(client + dwLocalPlayer);
+            std::cout << "[+] New Player Address: " << std::hex << localPlayer << std::endl;
+            while (GetAsyncKeyState(VK_INSERT)){}
+        }
+        flag = MemClass.readMem<BYTE>(localPlayer + m_fFlags);
+        if (GetAsyncKeyState(VK_SPACE) && flag & (1 << 0)) {
+            MemClass.writeMem<DWORD>(client + dwForceJump, 6);
+        }
+    
+        Sleep(1);
+    }
 }
